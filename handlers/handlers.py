b7b2b3338da06edc3e052f5daf3642d55bf247c4
maximkasigma777 <maksimzabrin@gmail.com>
2025-05-06 19:34:17 +0300

    u

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,4 +1,4 @@
-from aiogram import types, F, Router
+from aiogram import types, F, Router, Dispatcher
 from aiogram.filters import Command
 from aiogram.enums import ParseMode
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
@@ -7,14 +7,14 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 router = Router()
 router_for_translate = Router()
-
+dp = Dispatcher()
 
 def main():
     # /start
     @router.message(F.text, Command('start'))
     async def start(message: types.Message):
         builder = ReplyKeyboardBuilder()
-        buttons = []
+        buttons = ['a', 'b']
         for i in buttons:
             builder.add(types.KeyboardButton(text=str(i)))
         builder.adjust(2)
@@ -23,5 +23,8 @@ def main():
 
         await message.answer(text=text, reply_markup=builder.as_markup(resize_keyboard=True))
 
+    @router.message(F.text, Command('help'))
+    async def help(message: types.Message):
+        await message.reply(f'Для использования бота, пожалуйста, выберите одну из кнопок которые вам нужны.')
 
 main()
