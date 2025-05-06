24d7445373b185e34f88d722e10fbe63d7bbde32
maximkasigma777 <maksimzabrin@gmail.com>
2025-05-06 20:31:04 +0300

    u

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,12 +1,13 @@
 from aiogram import types, F, Router, Dispatcher
-from aiogram.filters import Command
+from aiogram.filters import Command, StateFilter
 from aiogram.enums import ParseMode
+from aiogram.fsm.context import FSMContext
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 # from bot.bot import USERS
 
 router = Router()
-router_for_translate = Router()
+
 
 def main():
     # /start
@@ -25,5 +26,26 @@ def main():
     @router.message(F.text, Command('help'))
     async def help(message: types.Message):
         await message.reply(f'Для использования бота, пожалуйста, выберите одну из кнопок которые вам нужны.')
+
+    class photochooser(StatesGroup):
+        photo_choosing = State()
+
+        @router.message(StateFilter(None), F.text == "a")
+        async def chooser(message: types.Message, state: FSMContext):
+            await message.answer(
+                text="Выберите фото"
+            )
+            await state.set_state(photo_choosing)
+
+        @router.message(
+            photochooser.photo_choosing,
+            F.photo
+        )
+        async def photo(message: Message, state: FSMContext, bot: Bot):
+            await bot.download(
+                message.photo[-1],
+                destination=f"/tmp/{message.photo[-1].file_id}.jpg"
+            )
+
 
 main()
